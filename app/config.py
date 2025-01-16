e39a3e7b088b2342a2b2468351a18dfc0d4c0c56
Pod <54603142+mblaster00@users.noreply.github.com>
2025-01-16 01:39:48 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # app.config.from_object('config')
 
 # MySQL configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Vegeta#5@localhost/brunonlp$online_retail'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://"username":"password"5@localhost/brunonlp$online_retail'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Secret key for session management
